1464242e8d24557b2ea93d58fafcf530b1e9b508
Ellen <evwhite2@gmail.com>
2019-10-28 13:44:00 -0500

    weather icons added

--- assets/app.js
+++ assets/app.js
@@ -30,15 +30,20 @@ function success(pos) {
         url: instantBase,
         method: "GET"
     }).then(function(response){
-    
+        console.log(response);
         //current weather updated when page loads
         currentTemp=response.main.temp;
         currentTemp= Math.round((currentTemp-273.5)*1.8+32);
+        var cImg = response.weather[0].icon;
+        console.log(cImg)
+        var cImgURL= "http://openweathermap.org/img/w/" +cImg + ".png";
+
         $("#cityName").text(response.name);
         $("#currentTemp").text(currentTemp+"°F");
         $("#currentHumid").text(response.main.humidity+"%");
-        $("#currentWind"+"mph").text(response.wind.speed);
+        $("#currentWind").text(response.wind.speed+" mph");
         $("#currentUV").text(response.coord.value);
+        $("#currentImg").attr("src", cImgURL);
      
     })
 
@@ -68,50 +73,65 @@ function success(pos) {
         var fcTempInfo0= byDay[0].main.temp;
         fcTempInfo0= Math.round((fcTempInfo0-273.5)*1.8+32);
         var fcHumidInfo0= byDay[0].main.humidity;
-        var fcIcon0= byDay[0].weather.icon;
-
+        var fcIcon0= byDay[0].weather[0].icon;
         var iconURL0 = "http://openweathermap.org/img/w/" +fcIcon0 + ".png";
         
         $("#forecastDateInfo0").text(fcDateInfo0);
         $("#forecastTemp0").text(fcTempInfo0+"°F");
         $("#forecastHumid0").text(fcHumidInfo0+"%");
-        $("#forecastIMG-4").attr("src", iconURL0);
+        $("#forecastIMG-0").attr("src", iconURL0);
 
         var fcDateInfo1= byDay[1].dt;
         var fcTempInfo1=byDay[1].main.temp;
         fcTempInfo1= Math.round((fcTempInfo1-273.5)*1.8+32);
         var fcHumidInfo1= byDay[1].main.humidity;
+        var fcIcon1= byDay[1].weather[0].icon;
+        var iconURL1 = "http://openweathermap.org/img/w/" +fcIcon1 + ".png";
+        
 
         $("#forecastDateInfo1").text(fcDateInfo1);
         $("#forecastTemp1").text(fcTempInfo1+"°F");
         $("#forecastHumid1").text(fcHumidInfo1+"%");
+        $("#forecastIMG-1").attr("src", iconURL1);
 
         var fcDateInfo2= byDay[2].dt;
         var fcTempInfo2= byDay[2].main.temp;
         fcTempInfo2= Math.round((fcTempInfo2-273.5)*1.8+32);
         var fcHumidInfo2= byDay[2].main.humidity;
+        var fcIcon2= byDay[2].weather[0].icon;
+        var iconURL2 = "http://openweathermap.org/img/w/" +fcIcon2 + ".png";
+        
 
         $("#forecastDateInfo2").text(fcDateInfo2);
         $("#forecastTemp2").text(fcTempInfo2+"°F");
         $("#forecastHumid2").text(fcHumidInfo2+"%");
+        $("#forecastIMG-2").attr("src", iconURL2);
 
         var fcDateInfo3= byDay[3].dt;
         var fcTempInfo3= byDay[3].main.temp;
         fcTempInfo3= Math.round((fcTempInfo3-273.5)*1.8+32);
         var fcHumidInfo3= byDay[3].main.humidity;
+        var fcIcon3= byDay[3].weather[0].icon;
+        var iconURL3 = "http://openweathermap.org/img/w/" +fcIcon3 + ".png";
+        
 
         $("#forecastDateInfo3").text(fcDateInfo3);
         $("#forecastTemp3").text(fcTempInfo3+"°F");
         $("#forecastHumid3").text(fcHumidInfo3+"%");
+        $("#forecastIMG-3").attr("src", iconURL3);
 
         var fcDateInfo4= byDay[4].dt;
         var fcTempInfo4=byDay[4].main.temp;
         fcTempInfo4= Math.round((fcTempInfo4-273.5)*1.8+32);
         var fcHumidInfo4= byDay[4].main.humidity;
+        var fcIcon4= byDay[4].weather[0].icon;
+        var iconURL4 = "http://openweathermap.org/img/w/" +fcIcon4 + ".png";
+        
 
         $("#forecastDateInfo4").text(fcDateInfo4);
         $("#forecastTemp4").text(fcTempInfo4+"°F");
         $("#forecastHumid4").text(fcHumidInfo4+"%");
+        $("#forecastIMG-4").attr("src", iconURL4);
 
 
         //end for loop
